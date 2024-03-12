b8bc5bc9fd3f28d25e6a975c55f0bff4dc2d1d5e
maria <m.petretckaia@ackerpause.de>
2024-03-12 20:44:05 +0100

    add navigation

--- src/routes/apartment-guide/+page.svelte
+++ src/routes/apartment-guide/+page.svelte
@@ -1,102 +1,212 @@
-<header class="intro-box">
-	<h1 class="intro-title">Wohnung Do & Don't Guide.</h1>
-	<div class="goup margin-bottom-small">
-		<img class="context-image" src="https://icons.iconarchive.com/icons/iconsmind/outline/128/Location-2-icon.png">
-		<p class="intro-text">Bindergasse 3 88131 Lindau,
-			<span class="text-bold">zweiten Etage rechts.</span>
-		</p>
+<div class="container">
+	<div class="row mt-3">
+		<h3>Wohnung Do & Don't Guide.</h3>
 	</div>
-	<div class="goup margin-bottom-small">
-		<img class="context-image" src="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-9/128/Accept-icon.png">
-		<p class="intro-text">Bitte zieht die Schuhe aus, wenn ihr die Wohnung betretet.</p>
-	</div>
-	<div class="goup margin-bottom-small">
-		<img class="context-image" src="https://icons.iconarchive.com/icons/ionic/ionicons/128/wifi-icon.png">
-		<p class="intro-text">Das WLAN-Netzwerk heißt <span class="text-bold">Martin</span>. Zugangsdaten findet ihr im Flur am Telefonregal.</p>
-	</div>
-</header>
-<section class="apartment-rooms">
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Flur</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Household-Wardrobe-icon.png">
-			<p class="intro-text">Ihr könnt eure Kleidung in Schrank aufhängen.
-				<br>Im Schrank findet ihr 2 Regenschirme, die ihr gerne nutzen dürft.</p>
+	<div class="row mt-2">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
 		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Household-Commode-icon.png">
-			<p class="intro-text">In der Schuhkommode könnt ihr eure Schuhe abstellen.</p>
-		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/roundicons/100-free-solid/128/vacuum-cleaner-icon.png">
-			<p class="intro-text">Ein Staubsauger und ein Wischmopp stehen euch zu Verfügung.</p>
+		<div class="col-6">
+			<p>Bitte zieht die Schuhe aus, wenn ihr die Wohnung betretet.</p>
 		</div>
 	</div>
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Küche</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-9/128/Accept-icon.png">
-			<p class="intro-text">Alle Zutaten zur Zubereitung von Müsli sowie alles im Kühlschrank dürft ihr gerne nutzen.</p>
+	<div class="row border-bottom">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wifi" viewBox="0 0 16 16">
+				<path d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.44 12.44 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.52.52 0 0 0 .668.05A11.45 11.45 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049"/>
+				<path d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.46 9.46 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065m-2.183 2.183c.226-.226.185-.605-.1-.75A6.5 6.5 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.5 5.5 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091zM9.06 12.44c.196-.196.198-.52-.04-.66A2 2 0 0 0 8 11.5a2 2 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z"/>
+			</svg>
 		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/danrabbit/elementary/128/Button-stop-icon.png">
-			<p class="intro-text">Messer, Töpfe und Pfannen <span class="text-bold">NICHT</span> in der Geschirrspülmaschine waschen.</p>
+		<div class="col">
+			<p >Das WLAN-Netzwerk heißt <span class="text-bold">Martin</span>. Zugangsdaten findet ihr im Flur am Telefonregal.</p>
+		</div >
+	</div>
+<!--Rooms-->
+<!--	Flur-->
+<div class="row mt-4 mb-2">
+	<h4>Flur</h4>
+</div>
+<div class="row">
+	<div class="col-auto pe-2">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+			<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+		</svg>
+	</div>
+	<div class="col">
+		<p>Ihr könnt eure Kleidung in Schrank aufhängen.
+		Im Schrank findet ihr 2 Regenschirme, die ihr gerne nutzen dürft</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-auto pe-2">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+			<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+		</svg>
+	</div>
+	<div class="col">
+		<p>In der Schuhkommode könnt ihr eure Schuhe abstellen</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-auto pe-2">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+			<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+		</svg>
+	</div>
+	<div class="col">
+		<p>Ein Staubsauger und ein Wischmopp stehen euch zu Verfügung.</p>
+	</div>
+</div>
+	<!--	Küche-->
+	<div class="row mt-4 mb-2">
+		<h4>Küche</h4>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
 		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/iconoir-team/iconoir/128/dishwasher-icon.png">
-			<p class="intro-text">Geschirrspülmitte findet ihr im Vorratsschrank im Flur.</p>
-		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Household-Washing-Machine-icon.png">
-			<p class="intro-text">Waschmittel findetn ihr im Vorratsschrank im Flur.</p>
+		<div class="col">
+			<p>Alle Zutaten zur Zubereitung von Müsli sowie alles im Kühlschrank dürft ihr gerne nutzen.</p>
 		</div>
 	</div>
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Badezimmer</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/pictogrammers/material/128/wardrobe-outline-icon.png">
-			<p class="intro-text">Das zusätzliche Toilettenpapier finted ihr im Badezimmerschrank.</p>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red"  class="bi bi-x-lg" viewBox="0 0 16 16">
+				<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+			</svg>
 		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/fa-team/fontawesome/128/FontAwesome-Pump-Soap-icon.png">
-			<p class="intro-text">Shampoo, Spülung, Duschgel, Seife könnt ihr gerne nutzen.</p>
-		</div>
-	</div>
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Esszimmer</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Household-Commode-icon.png">
-			<p class="intro-text">Weitere Handtücher findet ihr in der ersten und zweiten Schublade von unten.</p>
-		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Music-Radio-1-icon.png">
-			<p class="intro-text">Radio "Lindau" könnt ihr gerne hören.</p>
-		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/pictogrammers/material/128/robot-vacuum-icon.png">
-			<p class="intro-text">Staubsaugroboter könnte ihr gerne nutzen.</p>
+		<div class="col">
+			<p>Messer, Töpfe und Pfannen <span class="text-bold">NICHT</span> in der Geschirrspülmaschine waschen.</p>
 		</div>
 	</div>
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Wohnzimmer</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/pictogrammers/material/128/sofa-outline-icon.png">
-			<p class="intro-text">Auf dem Sofa <span class="text-bold">NICHT</span> essen.</p>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
 		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/icons8/windows-8/128/Household-Tv-icon.png">
-			<p class="intro-text">Netflix and Amazon prime könnt ihr gerne anschauen.</p>
-		</div>
-	</div>
-	<div class="room-box margin-bottom-medium">
-		<h4 class="title-small margin-bottom-small">Schlafzimmer</h4>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/ionic/ionicons/128/bed-outline-icon.png">
-			<p class="intro-text">Saubere Bettwäsche und 2 Sets Handtücher werden auf dem Bett bereitgestellt.</p>
-		</div>
-		<div class="goup margin-bottom-small">
-			<img class="context-image" src="https://icons.iconarchive.com/icons/pictogrammers/material/128/wardrobe-outline-icon.png">
-			<p class="intro-text">Wir werden eine Sektion des Schranks für euch freimachen.</p>
+		<div class="col">
+			<p>Geschirrspülmitte findet ihr im Vorratsschrank im Flur.</p>
 		</div>
 	</div>
-</section>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Waschmittel findetn ihr im Vorratsschrank im Flur.</p>
+		</div>
+	</div>
+	<!--	Badezimmer-->
+	<div class="row mt-4 mb-2">
+		<h4>Badezimmer</h4>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Das zusätzliche Toilettenpapier finted ihr im Badezimmerschrank.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Shampoo, Spülung, Duschgel, Seife könnt ihr gerne nutzen.</p>
+		</div>
+	</div>
+	<!--	Esszimmer-->
+	<div class="row mt-4 mb-2">
+		<h4>Esszimmer</h4>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Weitere Handtücher findet ihr in der ersten und zweiten Schublade von unten.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Radio "Lindau" könnt ihr gerne hören</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Staubsaugroboter könnte ihr gerne nutzen.</p>
+		</div>
+	</div>
+	<!--	Wohnzimmer-->
+	<div class="row mt-4 mb-2">
+		<h4>Wohnzimmer</h4>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red"  class="bi bi-x-lg" viewBox="0 0 16 16">
+				<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Auf dem Sofa <span class="text-bold">NICHT</span> essen.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Netflix and Amazon prime könnt ihr gerne anschauen.</p>
+		</div>
+	</div>
+	<!--	Schlafzimmer-->
+	<div class="row mt-4 mb-2">
+		<h4>Schlafzimmer</h4>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Saubere Bettwäsche und 2 Sets Handtücher werden auf dem Bett bereitgestellt.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-auto pe-2">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+				<path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+			</svg>
+		</div>
+		<div class="col">
+			<p>Wir werden eine Sektion des Schranks für euch freimachen.</p>
+		</div>
+	</div>
+</div>
