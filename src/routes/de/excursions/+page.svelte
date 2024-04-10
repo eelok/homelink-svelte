485added68e04fc43696ac2ce3403cee4aca176f
Tim Tkachenko <wassertim@gmail.com>
2024-04-10 19:32:58 +0000

    add radtours

---
+++ src/routes/de/excursions/+page.svelte
@@ -0,0 +1,6 @@
+<div class="container">
+	<h1>Fahrradtours</h1>
+
+	<p>Wir haben für euch einige Fahrradtours zusammengestellt, die ihr in Lindau und Umgebung unternehmen könnt.</p>
+	<iframe src="https://www.komoot.com/collection/2657843/embed" width="100%" height="580" frameborder="0" scrolling="no"></iframe>
+</div>
