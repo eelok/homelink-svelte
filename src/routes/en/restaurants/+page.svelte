7234d280c4d8c8fcb5c04bb6f981ba0f5757e629
Tim Tkachenko <wassertim@gmail.com>
2024-04-12 06:44:10 +0000

    update restaurants

--- src/routes/en/restaurants/+page.svelte
+++ src/routes/en/restaurants/+page.svelte
@@ -1,35 +1,59 @@
 <div class="container">
     <h1>Restaurants</h1>
     <h2>Lindau</h2>
-    <p>There are many good restaurants on <b>Lindau Island</b>. In google maps you can find a lot. Here are our favourite ones.</p>
+    <p>There are many excellent restaurants on <b>Lindau Island</b>. You can find a variety on Google Maps, but here are some of our favourites.</p>
     <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href="https://www.facebook.com/ThungthongThairestaurant.Lindau">Restaurant Thung Thong</a>. 
-            In the neighboring building, there is a good Thai restaurant, Thung Thong, which offers many delicious Thai dishes.
-        </li>
-        <li class="list-group-item"><a href="https://il-leone-pizza-bar.webador.de/">Pizzeria IL Leone</a>. 
-            The Pizzeria IL Leone on Vordere Metzgergasse 14 makes delicious pizzas.
-        </li>
-        <li class="list-group-item"><a href="https://streetkhazana.de/">Restaurant Street Khazana</a>. 
-            We also recommend the Indian restaurant Street Khazana (Schmiedgasse 8).
-        </li>
-        <li class="list-group-item"><a href="https://vinahaus-lindau.de/">Restaurant Vina Haus</a>. 
-            The Vietnamese restaurant Vina Haus (Langenweg 24). Not on the island, but nearby.
-        </li>
-        <li class="list-group-item"><a href="https://engel-lindau.de/gast-und-bierstube/">Restaurant Engel</a>. 
-            If you want to try regional cuisine, we recommend the Restaurant Engel (Schafgasse 4).
-        </li>
-        <li class="list-group-item"><a href="https://www.restaurant-zum-alten-rathaus-lindau.de/">Restaurant zum Alten Rathaus</a>. 
-            The Restaurant zum Alten Rathaus (Salzgasse 2) is also very good.
-        </li>
-    </ul>
-    <h2>Restaurants in Bregenz</h2>
-    <p>If you are in <b>Bregenz</b>, we recommend the following restaurants:</p>
-    <ul class="list-group list-group-flush">
+		<li class="list-group-item">
+			<a href="http://www.hamma.de/">Hamma Bäckerei</a> - A local bakery for your morning coffee and pastry, located on Maximilianstraße 11.
+			<a href="https://maps.app.goo.gl/1EhnHmXCNYVgyHFC8" target="_blank">View on map</a>.
+		</li>
         <li class="list-group-item">
-            <a href="https://www.sakurasushi.at/">Sakura Sushi Bar</a>. A restaurant with Japanese cuisine.
+            <a href="https://www.facebook.com/ThungthongThairestaurant.Lindau">Restaurant Thung Thong</a> - A delightful Thai restaurant in the neighboring building offering a variety of delicious Thai dishes. 
+            <a href="https://maps.app.goo.gl/7DRsAMk5UZsdPvBj7" target="_blank">View on map</a>.
         </li>
         <li class="list-group-item">
-            <a href="https://www.hotelweisseskreuz.at/goldener-hirschen/">Goldener Hirschen</a>. A traditional restaurant with Austrian cuisine.
+            <a href="https://www.facebook.com/Pizzeria-Piccola-997046647026182/info/?tab=overview">Pizzeria & Eiscafé Piccola</a> - Known for crafting delicious pizzas, located on Vordere Metzgergasse 14.
+            <a href="https://maps.app.goo.gl/hBZtB3ihRkHQysGJ7" target="_blank">View on map</a>.
+        </li>
+        <li class="list-group-item">
+            <a href="https://streetkhazana.de/">Restaurant Street Khazana</a> - A must-visit for lovers of Indian cuisine, located on Schmiedgasse 8.
+            <a href="https://maps.app.goo.gl/ow6AwjtQjF3NDupAA" target="_blank">View on map</a>.
+        </li>
+        <li class="list-group-item">
+            <a href="https://vinahaus-lindau.de/">Restaurant Vina Haus</a> - A Vietnamese restaurant off the island at Langenweg 24, offering traditional flavors in a nearby setting.
+            <a href="https://maps.app.goo.gl/iegYJTQZngEBwTdF8" target="_blank">View on map</a>.
+        </li>
+        <li class="list-group-item">
+            <a href="https://engel-lindau.de/gast-und-bierstube/">Restaurant Engel</a> - Ideal for those wanting to experience regional cuisine, located on Schafgasse 4.
+            <a href="https://maps.app.goo.gl/iSXYMAJwFPc2FynA9" target="_blank">View on map</a>.
+        </li>
+        <li class="list-group-item">
+            <a href="https://www.restaurant-zum-alten-rathaus-lindau.de/">Restaurant zum Alten Rathaus</a> - Highly recommended for its high-quality offerings, situated on Salzgasse 2.
+            <a href="https://maps.app.goo.gl/3mRvCqvTbpmRuhS66" target="_blank">View on map</a>.
         </li>
     </ul>
+    <h2>Bregenz</h2>
+    <p>If you find yourself in <b>Bregenz</b>, consider these highly recommended dining options:</p>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <a href="https://www.sakurasushi.at/">Sakura Sushi Bar</a> - Offers an authentic Japanese dining experience.
+            <a href="https://maps.app.goo.gl/gBjeAyWZDUTP3Fpy7" target="_blank">View on map</a>.
+        </li>
+        <li class="list-group-item">
+            <a href="https://www.hotelweisseskreuz.at/goldener-hirschen/">Goldener Hirschen</a> - A traditional restaurant renowned for its Austrian cuisine.
+            <a href="https://maps.google.com?q=Bregenz,Goldener%20Hirschen" target="_blank">View on map</a>.
+        </li>
+		<li class="list-group-item">
+			<a href="http://bahi.at/">BAHI café & coworking</a> - The best coffee in Bregenz.
+			<a href="https://maps.app.goo.gl/dn7Ta7vgLUXmaNds7" target="_blank">View on map</a>.
+		</li>
+		<li class="list-group-item">
+			<a href="https://www.xn--bckerei-mangold-0kb.at/">Backery Mangold</a> - Try the Bienenstichcroissant.
+			<a href="https://maps.app.goo.gl/aPtAM2LuuHY113rLA" target="_blank">View on map</a>.
+		</li>
+    </ul>
+	<h2>Michelin Star Restaurants</h2>
+	<p>
+		For gourmet enthusiasts, our region boasts several restaurants awarded with a red star in the <a href="https://guide.michelin.com/en/de/bayern/lindau/restaurants?showMap=true&sort=distance" target="_blank">Michelin Restaurant Guide</a>.
+	</p>
 </div>
