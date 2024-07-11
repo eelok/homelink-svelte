d7a5e877634d24ef194db6b354e86a0a7d36f272
Tim Tkachenko <wassertim@gmail.com>
2024-07-11 11:27:23 +0200

    add more info to excursions

--- src/routes/en/excursions/+page.svelte
+++ src/routes/en/excursions/+page.svelte
@@ -7,11 +7,18 @@
     <p>We have compiled some bike tours for you that you can undertake in and around Lindau.</p>
     <iframe src="https://www.komoot.com/collection/2657843/embed" width="100%" height="580" frameborder="0" scrolling="no"></iframe>
     <hr/>
+
+    <p>Here comes to the famous Bodensee-Radweg</p>
+    <iframe src="https://www.komoot.com/collection/41/embed" width="100%" height="580" frameborder="0" scrolling="no"></iframe>
+    <hr />
     <h2>Hiking Tours</h2>
 
-    <p>Explore the scenic beauty of Lindau and its surroundings on foot with these carefully curated hiking tours, designed to showcase the natural wonders of our area.</p>
+    <p>Explore the scenic beauty of Lindau and its surroundings on foot.</p>
     <iframe src="https://www.komoot.com/collection/2671708/embed" width="100%" height="580" frameborder="0" scrolling="no"></iframe>
-
+<hr/>
+    <p>Here comes to the Bodenseerundweg</p>
+    <iframe src="https://www.komoot.com/collection/1950360/embed" width="100%" height="580" frameborder="0" scrolling="no"></iframe>
+    <hr/>
     <h2>Additional Resources</h2>
     <p>For more hiking and biking tips, inspiration, and up-to-date information about exploring the Lake Constance region, check out our social media channels:</p>
     <ul>
@@ -19,4 +26,9 @@
         <li>YouTube: <a href="https://www.youtube.com/@lindau-island" target="_blank" rel="noopener noreferrer">Lindau Island</a> - Subscribe to our channel for scenic tours of the Lake Constance region.</li>
     </ul>
     <p>These resources are regularly updated with new content to help you make the most of your outdoor adventures during your stay.</p>
+    <h2>Official resources</h2>
+    <ul>
+        <li><a href="https://www.bodensee.eu/en">Lake Constance - Official Website</a></li>
+        <li><a href="https://www.lindau.de/">Lindau - Official Website</a></li>
+    </ul>
 </div>
