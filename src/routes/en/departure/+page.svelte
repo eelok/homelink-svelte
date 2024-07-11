eb236b169054d49dc200e4e6598e90fa7cf04955
Tim Tkachenko <wassertim@gmail.com>
2024-07-11 11:37:39 +0200

    add robot information

--- src/routes/en/departure/+page.svelte
+++ src/routes/en/departure/+page.svelte
@@ -2,10 +2,10 @@
     <h1>Departure Instructions</h1>
     <p>Thank you for staying with us! Before you leave, we kindly ask you to:</p>
     <ul>
-        <li>Leave the keys on the dining table.</li>
+        <li>Leave the keys in the keysafe.</li>
         <li>Remove the bed linen from the mattresses and leave it on top of each bed. You do not need to wash them.</li>
         <li>Switch off all the heaters (if you have turned any on).</li>
-        <li>Run the robot vacuum cleaner. Before you start, close the door to the bathroom and make sure you haven't left any cables on the floor. You don't need to wait until it finishes cleaning.</li>
+        <li>For you convinience you can run the robot vacuum cleaner. To start it, just push the middle button on the robot. Before you start, close the door to the bathroom and make sure you haven't left any cables on the floor. You don't need to wait until it finishes cleaning.</li>
         <li>Use the dishwasher to clean the dishes instead of washing them by hand. Simply place the dishes in the dishwasher and turn it on.</li>
     </ul>
     <p>We hope you had a wonderful stay and safe travels home!</p>
